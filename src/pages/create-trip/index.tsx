da08bf66452f5ceae67d1fc34209ee1aef85338a
AnaLara <analara714@gmail.com>
2024-07-09 22:10:24 -0300

    feat(invite-guest-step): extrai e componentiza o botao de adicionar pessoas convidadas do CreateTripPage

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -1,9 +1,9 @@
-import { ArrowRight, UserRoundPlus } from "lucide-react";
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { InviteGuestModal } from "./invite-guests-modal";
 import { ConfirmTripModal } from "./confirm-trip-modal";
 import { DestinationAndDateStep } from "./steps/destination-and-date-step";
+import { InviteGuestsStep } from "./steps/invite-guests-step";
 
 export function CreateTripPage() {
   const navigate = useNavigate();
@@ -80,32 +80,11 @@ export function CreateTripPage() {
             openGuestsInput={openGuestsInput}
           />
           {isGuestsInputOpen && (
-            <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-              <button
-                onClick={openGuestsModal}
-                type="button"
-                className="flex items-center gap-2 flex-1 text-left"
-              >
-                <UserRoundPlus className="size-5 text-zinc-400" />
-                {emailsToInvite.length > 0 ? (
-                  <span className="text-zinc-100">
-                    {emailsToInvite.length} pessoa(s) convidada(s)
-                  </span>
-                ) : (
-                  <span className="text-lg text-zinc-400 flex-1">
-                    Quem estará na viagem?
-                  </span>
-                )}
-              </button>
-              <div className="w-px h-6 bg-zinc-800"></div>
-              <button
-                onClick={openConfirmTripModal}
-                className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
-              >
-                Confirmar viagem
-                <ArrowRight className="size-5 " />
-              </button>
-            </div>
+            <InviteGuestsStep
+              emailsToInvite={emailsToInvite}
+              openConfirmTripModal={openConfirmTripModal}
+              openGuestsModal={openGuestsModal}
+            />
           )}
         </div>
         <p className="text-sm text-zinc-500">
@@ -114,7 +93,7 @@ export function CreateTripPage() {
           com nossos{" "}
           <a className="text-zinc-300 underline" href="#">
             termos de uso
-          </a>{" "}
+          </a>
           e{" "}
           <a className="text-zinc-300 underline" href="#">
             políticas de privacidade
