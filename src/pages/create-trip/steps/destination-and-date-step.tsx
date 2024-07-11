4ace07f0c75484d32d375bc2cabb90911a2add4f
AnaLara <analara714@gmail.com>
2024-07-11 15:30:46 -0300

    feat(destination-and-date-step): modal de inserir data com calendario

--- src/pages/create-trip/steps/destination-and-date-step.tsx
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -1,15 +1,41 @@
-import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
+import { ArrowRight, Calendar, MapPin, Settings2, X } from "lucide-react";
+import { DateRange, DayPicker } from "react-day-picker";
 import { Button } from "../../../components/button";
+import { useState } from "react";
 interface DestinationAndDateStepProps {
   isGuestsInputOpen: boolean;
   closeGuestsInput: () => void;
   openGuestsInput: () => void;
 }
+import "react-day-picker/dist/style.css";
+import { format } from "date-fns";
+
 export function DestinationAndDateStep({
   isGuestsInputOpen,
   closeGuestsInput,
   openGuestsInput,
 }: DestinationAndDateStepProps) {
+  const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+  const [eventStartAndEndDates, setEventStartAndEndDates] = useState<
+    DateRange | undefined
+  >();
+
+  const displayedDate =
+    eventStartAndEndDates &&
+    eventStartAndEndDates.from &&
+    eventStartAndEndDates.to
+      ? format(eventStartAndEndDates.from, "d ' de ' LLL").concat(
+          " até ".concat(format(eventStartAndEndDates.to, "d ' de ' LLL"))
+        )
+      : null;
+
+  function openDatePicker() {
+    setIsDatePickerOpen(true);
+  }
+  function closeDatePicker() {
+    setIsDatePickerOpen(false);
+  }
+
   return (
     <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
       <div className="flex items-center gap-2 flex-1">
@@ -21,15 +47,38 @@ export function DestinationAndDateStep({
           className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
         />
       </div>
-      <div className="flex items-center gap-2">
+      <button
+        onClick={openDatePicker}
+        disabled={isGuestsInputOpen}
+        className="flex items-center gap-2 text-left w-[240px]"
+      >
         <Calendar className="size-5 text-zinc-400" />
-        <input
-          type="text"
-          disabled={isGuestsInputOpen}
-          placeholder="Quando?"
-          className="bg-transparent text-lg placeholder-zinc-400 w-48 outline-none"
-        />
-      </div>
+        <span className="text-lg text-zinc-400 w-48 flex-1">
+          {displayedDate || "Quando?"}
+        </span>
+      </button>
+      {isDatePickerOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">Selecione a data</h2>
+                <button type="button" onClick={closeDatePicker}>
+                  <X className="size-5 text-zinc-400" />
+                </button>
+              </div>
+            </div>
+            <DayPicker
+              mode="range"
+              selected={eventStartAndEndDates}
+              modifiersClassNames={{
+                selected: "bg-lime-300 text-lime-950 hover:bg-lime-400",
+              }}
+              onSelect={setEventStartAndEndDates}
+            />
+          </div>
+        </div>
+      )}
       <div className="w-px h-6 bg-zinc-800"></div>
       {isGuestsInputOpen ? (
         <Button variant="secondary" onClick={closeGuestsInput}>
