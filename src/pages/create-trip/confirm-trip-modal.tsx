f1db3c95de312543fa1fed8cc24146ed6e662a29
AnaLara <analara714@gmail.com>
2024-07-10 21:55:12 -0300

    chore(create-trip): adiciona o componente global button nos arquivos da pagina de detalhes da viagem para melhorar o código

--- src/pages/create-trip/confirm-trip-modal.tsx
+++ src/pages/create-trip/confirm-trip-modal.tsx
@@ -1,5 +1,6 @@
 import { Plus, User, X } from "lucide-react";
 import { FormEvent } from "react";
+import { Button } from "../../components/button";
 
 interface ConfirmTripModalProps {
   closeConfirmTripModal: () => void;
@@ -51,13 +52,10 @@ export function ConfirmTripModal({
               className="bg-transparent text-lg placeholder-zinc-400 w-48 outline-none flex-1"
             />
           </div>
-          <button
-            type="submit"
-            className="bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400"
-          >
+          <Button size="full">
             Confirmar criação da viagem
             <Plus className="size-5 " />
-          </button>
+          </Button>
         </form>
       </div>
     </div>
